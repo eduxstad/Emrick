b31bd778ea26cf889b032718abb0d325804e296a
Erik Duxstad <eduxstad@gmail.com>
2022-09-29 00:05:20 -0400

    update schematic with usb to serial component
    ignore the changes to the lightdemo files

--- software/cc1310/LightDemo/Debug/subdir_rules.mk
+++ software/cc1310/LightDemo/Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/bin/armcl" -mv7M3 --code_state=16 --float_support=vfplib -me --include_path="C:/Users/eduxstad/workspace_v12/gpiointerrupt_CC1310_LAUNCHXL_nortos_ccs" --include_path="C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source" --include_path="C:/ti/simplelink_cc13x0_sdk_4_20_02_07/kernel/nortos" --include_path="C:/ti/simplelink_cc13x0_sdk_4_20_02_07/kernel/nortos/posix" --include_path="C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include" --define=DeviceFamily_CC13X0 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/bin/armcl" -mv7M3 --code_state=16 --float_support=vfplib -me --include_path="//NAS01.ITAP.PURDUE.EDU/puhome/My Documents/GitHub/Emrick/software/cc1310/LightDemo" --include_path="C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source" --include_path="C:/ti/simplelink_cc13x0_sdk_4_20_02_07/kernel/nortos" --include_path="C:/ti/simplelink_cc13x0_sdk_4_20_02_07/kernel/nortos/posix" --include_path="C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include" --define=DeviceFamily_CC13X0 -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
