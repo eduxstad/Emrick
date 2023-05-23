d072e434c836f17e702593445ccfd756c7a847ee
Erik Duxstad <eduxstad@gmail.com>
2023-05-23 13:49:04 -0500

    add battery monitor and receiver
    These sample project were created to demonstrate and test the ability of the cc1310 to monitor the battery voltage. The Battery Monitor project is a modified version of SPIWS that creates a second thread the periodically checks and sends a wireless update of the battery voltage to the Receiver, which is a modified version of ReceiverSPI that receives the update from the monitor and prints it to UART. The BatteryMonitor project demonstrates multiple threads and wireless updates while running the LED strips.

---
+++ software/cc1310/WirelessBatteryReceiver/rfPacketRx.c
@@ -0,0 +1,267 @@
+/*
+ * Copyright (c) 2019, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/***** Includes *****/
+/* Standard C Libraries */
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+/* TI Drivers */
+#include <ti/drivers/rf/RF.h>
+#include <ti/drivers/PIN.h>
+#include <ti/drivers/SPI.h>
+#include <ti/display/Display.h>
+#include <ti/display/DisplayUart.h>
+#include <ti/display/DisplayExt.h>
+
+/* POSIX Header files */
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+/* Driverlib Header files */
+#include DeviceFamily_constructPath(driverlib/rf_prop_mailbox.h)
+
+/* Board Header files */
+#include "Board.h"
+#include "WS2812.h"
+
+/* Application Header files */
+#include "RFQueue.h"
+#include "smartrf_settings/smartrf_settings.h"
+
+/***** Defines *****/
+
+/* Packet RX Configuration */
+#define DATA_ENTRY_HEADER_SIZE 8  /* Constant header size of a Generic Data Entry */
+#define MAX_LENGTH             20 /* Max length byte the radio will accept */
+#define NUM_DATA_ENTRIES       2  /* NOTE: Only two data entries supported at the moment */
+#define NUM_APPENDED_BYTES     2  /* The Data Entries data field will contain:
+                                   * 1 Header byte (RF_cmdPropRx.rxConf.bIncludeHdr = 0x1)
+                                   * Max 30 payload bytes
+                                   * 1 status byte (RF_cmdPropRx.rxConf.bAppendStatus = 0x1) */
+
+SPI_Handle      masterSpi;
+SPI_Params      spiParams;
+uint16_t        colorIndex = 0;
+
+
+#include <ti/drivers/SPI.h>
+#include <ti/drivers/GPIO.h>
+
+/***** Prototypes *****/
+static void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e);
+
+/***** Variable declarations *****/
+static RF_Object rfObject;
+static RF_Handle rfHandle;
+
+/* Pin driver handle */
+static PIN_Handle ledPinHandle;
+static PIN_State ledPinState;
+
+/* Buffer which contains all Data Entries for receiving data.
+ * Pragmas are needed to make sure this buffer is 4 byte aligned (requirement from the RF Core) */
+#if defined(__TI_COMPILER_VERSION__)
+#pragma DATA_ALIGN (rxDataEntryBuffer, 4);
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  MAX_LENGTH,
+                                                  NUM_APPENDED_BYTES)];
+#elif defined(__IAR_SYSTEMS_ICC__)
+#pragma data_alignment = 4
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  MAX_LENGTH,
+                                                  NUM_APPENDED_BYTES)];
+#elif defined(__GNUC__)
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  MAX_LENGTH,
+                                                  NUM_APPENDED_BYTES)]
+                                                  __attribute__((aligned(4)));
+#else
+#error This compiler is not supported.
+#endif
+
+/* UART Display */
+Display_Handle displayHandle;
+
+/* Receive dataQueue for RF Core to fill in data */
+static dataQueue_t dataQueue;
+static rfc_dataEntryGeneral_t* currentDataEntry;
+static uint8_t packetLength;
+static uint8_t* packetDataPointer;
+
+/* Array Index variables */
+uint16_t loc_u16_pixelIndex;
+uint16_t arrIdx;
+
+/* Board ID Number */
+uint16_t BoardID = 2;
+
+static uint32_t packet[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte is stored in a separate variable */
+uint8_t packet2[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte is stored in a separate variable */
+
+/*
+ * Application LED pin configuration table:
+ *   - All LEDs board LEDs are off.
+ */
+PIN_Config pinTable[] =
+{
+    Board_PIN_LED1 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+    PIN_TERMINATE
+};
+
+/***** Function definitions *****/
+sem_t masterSem;
+
+
+void *mainThread(void *arg0)
+{
+    RF_Params rfParams;
+    RF_Params_init(&rfParams);
+
+    /* Call driver init functions. */
+    Display_init();
+    GPIO_init();
+    SPI_init();
+
+
+    GPIO_setConfig(Board_SPI_MASTER_READY, GPIO_CFG_OUTPUT | GPIO_CFG_OUT_LOW);
+    GPIO_write(Board_SPI_MASTER_READY, 1);
+
+    displayHandle = Display_open(Display_Type_UART, NULL);
+    if (displayHandle == NULL) {
+        /* Display_open() failed */
+        while (1);
+    }
+
+    char reset_msg[] =
+            "========================\nBoard Reset\n========================";
+    Display_printf(displayHandle, DisplayUart_SCROLLING, 0, reset_msg);
+
+    /* Open LED pins */
+    ledPinHandle = PIN_open(&ledPinState, pinTable);
+    if (ledPinHandle == NULL)
+    {
+        while(1);
+    }
+
+    if( RFQueue_defineQueue(&dataQueue,
+                            rxDataEntryBuffer,
+                            sizeof(rxDataEntryBuffer),
+                            NUM_DATA_ENTRIES,
+                            MAX_LENGTH + NUM_APPENDED_BYTES))
+    {
+        /* Failed to allocate space for all data entries */
+        while(1);
+    }
+
+    /* Modify CMD_PROP_RX command for application needs */
+    /* Set the Data Entity queue for received data */
+    RF_cmdPropRx.pQueue = &dataQueue;
+    /* Discard ignored packets from Rx queue */
+    RF_cmdPropRx.rxConf.bAutoFlushIgnored = 1;
+    /* Discard packets with CRC error from Rx queue */
+    RF_cmdPropRx.rxConf.bAutoFlushCrcErr = 1;
+    /* Implement packet length filtering to avoid PROP_ERROR_RXBUF */
+    RF_cmdPropRx.maxPktLen = MAX_LENGTH;
+    RF_cmdPropRx.pktConf.bRepeatOk = 1;
+    RF_cmdPropRx.pktConf.bRepeatNok = 1;
+
+    /* Request access to the radio */
+#if defined(DeviceFamily_CC26X0R2)
+    rfHandle = RF_open(&rfObject, &RF_prop, (RF_RadioSetup*)&RF_cmdPropRadioSetup, &rfParams);
+#else
+    rfHandle = RF_open(&rfObject, &RF_prop, (RF_RadioSetup*)&RF_cmdPropRadioDivSetup, &rfParams);
+#endif// DeviceFamily_CC26X0R2
+
+    /* Set the frequency */
+    RF_postCmd(rfHandle, (RF_Op*)&RF_cmdFs, RF_PriorityNormal, NULL, 0);
+
+    /* Enter RX mode and stay forever in RX */
+    RF_EventMask terminationReason = RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropRx,
+                                               RF_PriorityNormal, &callback,
+                                               RF_EventRxEntryDone);
+
+    return (NULL);
+}
+
+void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
+{
+
+    if (e & RF_EventRxEntryDone)
+    {
+//        /* Toggle pin to indicate RX */
+//        PIN_setOutputValue(ledPinHandle, Board_PIN_LED1,
+//                           !PIN_getOutputValue(Board_PIN_LED1));
+
+        /* Get current unhandled data entry */
+        currentDataEntry = RFQueue_getDataEntry();
+
+        /* Handle the packet data, located at &currentDataEntry->data:
+         * - Length is the first byte with the current configuration
+         * - Data starts from the second byte */
+        packetLength      = *(uint8_t*)(&currentDataEntry->data);
+        packetDataPointer = (uint8_t*)(&currentDataEntry->data + 1);
+
+        /* Copy the payload + the status byte to the packet variable */
+        memcpy(packet, packetDataPointer, (packetLength + 1));
+
+        /* uint16_t i;
+        for(i = 0; i < sizeof(packet)/sizeof(packet[0]); i++) {
+            packet2[i] = packet[i];
+        } */
+
+        /* Battery Monitor */
+        uint32_t bat_measure;
+        float bat_voltage;
+
+        /* time (in seconds) delay */
+        uint32_t time;
+
+        bat_measure = packet[0];
+        bat_voltage = ((bat_measure >> 8) & 0x7)
+                + ((float) (bat_measure & 0xFF)) / 256.0;
+        time = packet[1];
+
+        Display_printf(displayHandle, DisplayUart_SCROLLING, 0, "[%d] Voltage: %f (%x, %x)", time, bat_voltage, packet[2], bat_measure);
+
+        RFQueue_nextEntry();
+    }
+
+}
